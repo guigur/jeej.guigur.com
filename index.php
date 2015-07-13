b870295f339be533bfb8c833c7e8e68008a35113
guigur <guigur13@gmail.com>
2015-07-13 16:44:19 +0200

    bug fixes

--- index.php
+++ index.php
@@ -7,24 +7,24 @@
         <body>
 <?php
 include_once('randomizer.php');
-echo '<video id="videoPlayer" width="100%" height="100%" src="'.$content.'" type="video/mp4" autoplay autobuffer controls ></video>';
+echo '<video id="videoPlayer" width="100%" height="100%" src="'.$content.'" type="video/mp4" autoplay autobuffer></video>';
 ?>
-<script type="text/javascript">
-    var nextVideo = "";
-    mdlol();
-    function mdlol()
-    {
-        $.ajax({url: "randomizer.php", success: function(result){
-            nextVideo  = result;
-            console.log(nextVideo);
-        }});
-    };
-    var videoPlayer = document.getElementById('videoPlayer');
-    videoPlayer.onended = function(){
-        videoPlayer.src = nextVideo;
-        mdlol();
-    }
-</script>
-
+            <script type="text/javascript">
+                var nextVideo = "";
+                mdlol();
+                function mdlol()
+                {
+                    $.ajax({url: "randomizer.php", success: function(result){
+                        nextVideo  = result;
+                        console.log(nextVideo);
+                    }});
+                }
+                var videoPlayer = document.getElementById('videoPlayer');
+                videoPlayer.onended = function()
+                {
+                    videoPlayer.src = nextVideo;
+                    mdlol();
+                }
+            </script>
         </body>
 </html>
